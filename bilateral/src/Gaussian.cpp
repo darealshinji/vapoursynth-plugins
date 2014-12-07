7630f9d4a3ffe9136725e28719d7784b0787ba41
darealshinji <djcj@gmx.de>
2014-12-07 13:21:38 +0100

    bilateral r3

--- bilateral/src/Gaussian.cpp
+++ bilateral/src/Gaussian.cpp
@@ -1,3 +1,22 @@
+/*
+* Bilateral filter - VapourSynth plugin
+* Copyright (C) 2014  mawen1250
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "Gaussian.h"
 
 
@@ -175,37 +194,51 @@ void Recursive_Gaussian_Parameters(const double sigma, FLType & B, FLType & B1, 
 
 void Recursive_Gaussian2D_Vertical(FLType * output, const FLType * input, int height, int width, int stride, const FLType B, const FLType B1, const FLType B2, const FLType B3)
 {
-    int i, j, lower, upper;
+    int i0, i1, i2, i3, j, lower, upper;
     FLType P0, P1, P2, P3;
-    int pcount = stride*height;
 
-    for (j = 0; j < width; j++)
+    if (output != input)
     {
-        lower = j;
-        upper = pcount;
+        memcpy(output, input, sizeof(FLType) * width);
+    }
 
-        i = lower;
-        output[i] = P3 = P2 = P1 = input[i];
+    for (j = 0; j < height; j++)
+    {
+        lower = stride * j;
+        upper = lower + width;
 
-        for (i += stride; i < upper; i += stride)
+        i0 = lower;
+        i1 = j < 1 ? i0 : i0 - stride;
+        i2 = j < 2 ? i1 : i1 - stride;
+        i3 = j < 3 ? i2 : i2 - stride;
+
+        for (; i0 < upper; i0++, i1++, i2++, i3++)
         {
-            P0 = B*input[i] + B1*P1 + B2*P2 + B3*P3;
-            P3 = P2;
-            P2 = P1;
-            P1 = P0;
-            output[i] = P0;
+            P3 = output[i3];
+            P2 = output[i2];
+            P1 = output[i1];
+            P0 = input[i0];
+            output[i0] = B*P0 + B1*P1 + B2*P2 + B3*P3;
         }
+    }
 
-        i -= stride;
-        P3 = P2 = P1 = output[i];
+    for (j = height - 1; j >= 0; j--)
+    {
+        lower = stride * j;
+        upper = lower + width;
 
-        for (i -= stride; i >= lower; i -= stride)
+        i0 = lower;
+        i1 = j >= height - 1 ? i0 : i0 + stride;
+        i2 = j >= height - 2 ? i1 : i1 + stride;
+        i3 = j >= height - 3 ? i2 : i2 + stride;
+
+        for (; i0 < upper; i0++, i1++, i2++, i3++)
         {
-            P0 = B*output[i] + B1*P1 + B2*P2 + B3*P3;
-            P3 = P2;
-            P2 = P1;
-            P1 = P0;
-            output[i] = P0;
+            P3 = output[i3];
+            P2 = output[i2];
+            P1 = output[i1];
+            P0 = output[i0];
+            output[i0] = B*P0 + B1*P1 + B2*P2 + B3*P3;
         }
     }
 }
@@ -217,7 +250,7 @@ void Recursive_Gaussian2D_Horizontal(FLType * output, const FLType * input, int 
 
     for (j = 0; j < height; j++)
     {
-        lower = stride*j;
+        lower = stride * j;
         upper = lower + width;
 
         i = lower;
