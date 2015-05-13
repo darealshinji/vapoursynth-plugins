7d4b8adda5e01446145bdab0104ac36eaa31ec29
darealshinji <djcj@gmx.de>
2015-05-13 21:22:02 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@ for d in . fluxsmooth mvtools nnedi3 tcomb imagereader/libjpeg-turbo ; do
     mkdir -p $d/m4 $d/build-aux
 done
 
+./configure.sh "$@" >/dev/null
 autoreconf -ivf
 autoreconf -ivf imagereader/libjpeg-turbo
 
