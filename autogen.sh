66e653d2afde51521ed78f2d48e0e3fb1737fcb4
Marshall Banana <djcj@gmx.de>
2015-05-14 17:05:20 +0200

    Initialize and update submodule with ./autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,11 @@ for d in . fluxsmooth mvtools nnedi3 tcomb imagereader/libjpeg-turbo ; do
     mkdir -p $d/m4 $d/build-aux
 done
 
+if [ -d ".git" ] && [ "x$(type -p git)" != "x" ]; then
+    git submodule init
+    git submodule update
+fi
+
 ./configure.sh "$@" >/dev/null
 autoreconf -ivf
 autoreconf -ivf imagereader/libjpeg-turbo
