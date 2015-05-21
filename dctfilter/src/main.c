849b624b638cc260fe02711fe4a5097b0625899f
Marshall Banana <djcj@gmx.de>
2015-05-22 00:51:46 +0200

    dctfilter: remove unused variable

--- dctfilter/src/main.c
+++ dctfilter/src/main.c
@@ -132,7 +132,6 @@ static void VS_CC PluginFree(void * a_pInstanceData, VSCore * a_pCore,
 	UNUSED(a_pCore);
 	UNUSED(a_cpVSAPI);
 
-	PluginData * pPluginData = (PluginData *)a_pInstanceData;
 	free(a_pInstanceData);
 }
 
