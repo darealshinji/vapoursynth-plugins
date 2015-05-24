c876ddbfa6c5b28fef807b7b7742370a2583ffa5
darealshinji <djcj@gmx.de>
2015-05-24 03:22:48 +0200

    Run autoreconf on ffms2/src

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-which git 2>/dev/null >/dev/null
-exitCode="$(echo $?)"
-if [ $exitCode = 0 ] && [ -d ".git" ]; then
-   git submodule init
-   git submodule update
-fi
-
 echo "autoconf"; autoconf
 echo "automake --add-missing --copy"; automake --add-missing --copy 2>/dev/null
 
 echo "autoreconf --install imagereader/libjpeg-turbo"
 autoreconf --install imagereader/libjpeg-turbo 2>/dev/null
 
+which git 2>/dev/null >/dev/null
+exitCode="$(echo $?)"
+if [ $exitCode = 0 ] && [ -d ".git" ]; then
+   git submodule init
+   git submodule update
+   echo "autoreconf --install ffms2/src"
+   autoreconf --install ffms2/src 2>/dev/null
+fi
