e3919808baf0b4a5d84c8e42a79246a2c9bbb409
Marshall Banana <djcj@gmx.de>
2015-05-24 03:47:42 +0200

    Fix installation

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-echo "autoconf"; autoconf
-echo "automake --add-missing --copy"; automake --add-missing --copy 2>/dev/null
+echo "autoconf"; autoconf 2>/dev/null >/dev/null
+echo "automake --add-missing --copy"; automake --add-missing --copy 2>/dev/null >/dev/null
 
 echo "autoreconf --install imagereader/libjpeg-turbo"
-autoreconf --install imagereader/libjpeg-turbo 2>/dev/null
+autoreconf --install imagereader/libjpeg-turbo 2>/dev/null >/dev/null
 
 which git 2>/dev/null >/dev/null
 exitCode="$(echo $?)"
 if [ $exitCode = 0 ] && [ -d ".git" ]; then
    git submodule init
    git submodule update
+fi
+
+if [ -f "ffms2/src/configure.ac" ]; then
    echo "autoreconf --install ffms2/src"
-   autoreconf --install ffms2/src 2>/dev/null
+   autoreconf --install ffms2/src 2>/dev/null >/dev/null
 fi
