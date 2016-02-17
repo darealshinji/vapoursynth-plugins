b85bc721cea2b22ae74704cdaf72c729bd18d44d
darealshinji <djcj@gmx.de>
2016-02-17 11:44:44 +0100

    BM3D r5

--- plugins/bm3d/src/Helper.h
+++ plugins/bm3d/src/Helper.h
@@ -104,6 +104,15 @@ private:
 #endif
 
 
+#if defined(__SSE2__)
+inline __m128 _mm_abs_ps(const __m128 &x)
+{
+    static const __m128 mask = _mm_castsi128_ps(_mm_set1_epi32(~0x80000000));
+    return _mm_and_ps(x, mask);
+}
+#endif
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Exception handle
 
@@ -115,19 +124,6 @@ private:
 #define DEBUG_BREAK exit(EXIT_FAILURE);
 #define DEBUG_FAIL(mesg) std::cerr << mesg << std::endl; std::cin.get();
 #endif
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Convert to std::string
-
-
-template < typename _Ty >
-std::string GetStr(const _Ty &src)
-{
-    std::stringstream ss;
-    ss << src;
-    return ss.str();
-}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
