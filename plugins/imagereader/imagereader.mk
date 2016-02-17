81e9e2f7a110c292880bfcf0f939f87af4e681ef
darealshinji <djcj@gmx.de>
2016-02-17 14:33:08 +0100

    Update libjpeg-turbo to v1.4.2

--- plugins/imagereader/imagereader.mk
+++ plugins/imagereader/imagereader.mk
@@ -1,7 +1,10 @@
 
 LIBNAME = imagereader
 local_CFLAGS = -Wno-sign-compare -I./libjpeg-turbo
-LIBADD = libjpeg-turbo/.libs/libturbojpeg.a -ljpeg -lpng -lz
+LIBADD = libjpeg-turbo/.libs/libturbojpeg.a \
+	libjpeg-turbo/.libs/libjpeg.a \
+	libjpeg-turbo/simd/.libs/libsimd.a \
+	-lpng -lz
 
 include ../../cc.inc
 
