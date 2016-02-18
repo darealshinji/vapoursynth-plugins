93b1ac6d18f7a852d1a754c72aafa8f1aa945311
darealshinji <djcj@gmx.de>
2016-02-18 13:08:39 +0100

    Update extra-plugins packaging files

--- vapoursynth-extra-plugins/debian/get-orig-source.sh
+++ vapoursynth-extra-plugins/debian/get-orig-source.sh
@@ -7,21 +7,8 @@ cd $src
 
 VERSION=$(git log -1 --format=%ci | head -c10 | sed -e 's/-//g')
 
-git submodule init
-git submodule update
-rm -rf ffms2/.git .git
-
-echo '' > nnedi3/src/nnedi3_weights.bin
-echo '' > waifu2x-models/anime_style_art/scale2.0x_model.json
-echo '' > waifu2x-models/anime_style_art/noise2_model.json
-echo '' > waifu2x-models/anime_style_art/noise1_model.json
-echo '' > waifu2x-models/anime_style_art_rgb/scale2.0x_model.json
-echo '' > waifu2x-models/anime_style_art_rgb/noise2_model.json
-echo '' > waifu2x-models/anime_style_art_rgb/noise1_model.json
-echo '' > waifu2x-models/photo/scale2.0x_model.json
-echo '' > waifu2x-models/photo/noise2_model.json
-echo '' > waifu2x-models/photo/noise1_model.json
-
+./autogen.sh
+rm -rf model-weights
 cd ..
 
 tar cvfJ vapoursynth-extra-plugins_${VERSION}.orig.tar.xz $src
