89e4e4e3ebb9870ac69280c87c7acb254274ab72
darealshinji <djcj@gmx.de>
2016-02-19 10:26:29 +0100

    Update vapoursynth-extra-plugins packaging

--- vapoursynth-extra-plugins/debian/get-orig-source.sh
+++ vapoursynth-extra-plugins/debian/get-orig-source.sh
@@ -8,7 +8,7 @@ cd $src
 VERSION=$(git log -1 --format=%ci | head -c10 | sed -e 's/-//g')
 
 ./autogen.sh
-rm -rf model-weights
+rm -rf model-weights .git
 cd ..
 
 tar cvfJ vapoursynth-extra-plugins_${VERSION}.orig.tar.xz $src
