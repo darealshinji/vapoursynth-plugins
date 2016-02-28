3536d0fe69cf29f261d155ed16f9f25f40d9315b
darealshinji <djcj@gmx.de>
2016-02-28 01:58:33 +0100

    waifu2x-models: update get-orig-source.sh

--- vapoursynth-waifu2x-models/debian/get-orig-source.sh
+++ vapoursynth-waifu2x-models/debian/get-orig-source.sh
@@ -4,8 +4,10 @@ VERSION="$(dpkg-parsechangelog -SVersion | cut -d- -f1)"
 
 git clone --depth 1 "https://github.com/darealshinji/vapoursynth-plugins.git" tmp
 
-mv tmp/waifu2x-models models
-rm -rf tmp
+for d in anime_style_art anime_style_art_rgb photo ; do
+    mkdir -p models/$d
+    cp tmp/models/$d/*.json models/$d
+done
 
 tar cvfJ vapoursynth-waifu2x-models_${VERSION}.orig.tar.xz models
 
