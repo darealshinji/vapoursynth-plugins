959f0f5e4273e95d744a0aa755a63267500cc091
darealshinji <djcj@gmx.de>
2016-03-06 02:55:22 +0100

    Updates

--- vapoursynth-extra-plugins/debian/get-orig-source.sh
+++ vapoursynth-extra-plugins/debian/get-orig-source.sh
@@ -2,13 +2,15 @@
 
 src=vapoursynth-extra-plugins-src
 
-git clone "https://github.com/darealshinji/vapoursynth-plugins.git" $src
+git clone --depth 1 "https://github.com/darealshinji/vapoursynth-plugins.git" $src
 cd $src
 
 VERSION=$(git log -1 --format=%ci | head -c10 | sed -e 's/-//g')
 
 ./autogen.sh
 rm -rf models .git autom4te.cache plugins/imagereader/libjpeg-turbo/autom4te.cache
+rm -f .gitignore autogen-cleanup.sh
+cp -f copyright ..
 cd ..
 
 tar cvfJ vapoursynth-extra-plugins_${VERSION}.orig.tar.xz $src
