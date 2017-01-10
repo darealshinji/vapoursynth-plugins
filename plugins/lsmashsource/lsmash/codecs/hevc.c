dc8cf90c331a7d0795bdb6383bf11fd59d6237f2
darealshinji <djcj@gmx.de>
2017-01-10 09:26:23 +0100

    Update lsmash (v2.12.4)

--- plugins/lsmashsource/lsmash/codecs/hevc.c
+++ plugins/lsmashsource/lsmash/codecs/hevc.c
@@ -2671,7 +2671,9 @@ int hevc_move_pending_hvcC_param
     info->hvcC_param.parameter_arrays = parameter_arrays;
     /* No pending hvcC. */
     lsmash_destroy_hevc_parameter_arrays( &info->hvcC_param_next );
+    uint8_t lengthSizeMinusOne = info->hvcC_param_next.lengthSizeMinusOne;
     memset( &info->hvcC_param_next, 0, sizeof(lsmash_hevc_specific_parameters_t) );
+    info->hvcC_param_next.lengthSizeMinusOne = lengthSizeMinusOne;
     info->hvcC_pending = 0;
     return 0;
 }
@@ -3039,6 +3041,8 @@ int hevc_print_codec_specific
         {
             uint16_t nalUnitLength = lsmash_bs_get_be16( bs );
             lsmash_bs_skip_bytes( bs, nalUnitLength );
+            lsmash_ifprintf( fp, array_indent, "nalUnit[%"PRIu16"]\n", j );
+            lsmash_ifprintf( fp, array_indent + 1, "nalUnitLength = %"PRIu16"\n", nalUnitLength );
         }
     }
     lsmash_bs_cleanup( bs );
